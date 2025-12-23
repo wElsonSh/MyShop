9f2cca2d3be0476937651090bb4e2046933885df
Daniel Shevel <seveldaiil@gmail.com>
2025-12-23 23:58:39 +0300

    products generation + basket icon

--- js/main.js
+++ js/main.js
@@ -41,3 +41,24 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // filters logic
+
+const products = [['apple', 10], ['banana', 20], ['alksdmklasmlk', 20], ['slslslslsl', 20], ['bananvavavavaa', 20], [';aspjasnjk', 20], ['apple', 10], ['banana', 20], ['alksdmklasmlk', 20], ['slslslslsl', 20], ['bananvavavavaa', 20], [';aspjasnjk', 20]]
+    , assortment_products = document.getElementById("assortment_products")
+products.forEach((product) => {
+    let product_card = document.createElement("li")
+        , product_card_container = document.createElement("div")
+        , product_card_title = document.createElement("h3")
+        , product_card_price = document.createElement("p")
+        , product_card_img = document.createElement("span")
+        , product_card_btn = document.createElement("button")
+    product_card_container.classList.add("assortment_product_card_container")
+    product_card_img.classList.add("assortment_product_card_img")
+    product_card_title.textContent = `${product[0]} :`
+    product_card_price.textContent = `$${product[1]}`
+    product_card_btn.textContent = 'buy'
+    product_card_container.append(product_card_title, product_card_price)
+    product_card.append(product_card_img, product_card_container, product_card_btn)
+    product_card.classList.add("assortment_product_card")
+    assortment_products.appendChild(product_card)
+})
+
